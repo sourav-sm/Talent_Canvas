2abee5f104dc9794c651e9b5c47d88084b1ec1e3
Sourav <souravmohanta4428@gmail.com>
2023-08-27 22:12:54 +0530

    update work section

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+//     var tabcontents = document.getElementsByClassName("tab-contents");
+
+//     function opentab(tabname,event){
+//       for(tablink of tablinks){
+//         tablink.classList.remove("active-link");
+//       }
+//       for(tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-tab");
+//       }
+//       event.currentTarget.classList.add("active-link");
+//       document.getElementById(tabname).classList.add("active-tab")
+//     }
+// // <!--for menu-->
+//     var sidemeu = document.getElementById("sidemenu");
+
+//     function openmenu(){
+//         sidemeu.style.right = "0";
+//     }
+//     function closemenu(){
+//         sidemeu.style.right = "-200px";
+//     }
+
 var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-    function opentab(tabname){
-      for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-      }
-      for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-      }
-      event.currentTarget.classList.add("active-link");
-      document.getElementById(tabname).classList.add("active-tab")
-    }
+function opentab(tabname, event) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
 // <!--for menu-->
-    var sidemeu = document.getElementById("sidemenu");
+var sidemeu = document.getElementById("sidemenu");
 
-    function openmenu(){
-        sidemeu.style.right = "0";
-    }
-    function closemenu(){
-        sidemeu.style.right = "-200px";
-    }
+function openmenu() {
+  sidemeu.style.right = "0";
+}
+
+function closemenu() {
+  sidemeu.style.right = "-200px";
+}
